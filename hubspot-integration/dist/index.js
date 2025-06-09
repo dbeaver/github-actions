329ccc84e5e6b90a4af956db0c5a64ef468e2a41
mayerro <roman.kunitskiy@gmail.com>
2025-06-09 10:23:23 +0200

    dbeaver/dbeaver-devops#1889 json

--- hubspot-integration/dist/index.js
+++ hubspot-integration/dist/index.js
@@ -31890,8 +31890,9 @@ async function main() {
   //     issue_number: github.context.payload.number
   // });
 
-  var searchRequest = { "inputs": [{ "id": context.issue.url }]}
-  const hs_search_request = await hs_request(hubspotUssueApi, searchRequest);
+  var searchRequestString = `{ 'inputs': [{ 'id': ${context.issue.url} }]}`
+  const searchRequestJson = JSON.parse(json);
+  const hs_search_request = await hs_request(hubspotUssueApi, searchRequestJson);
   console.log(hs_search_request)
 }
 
